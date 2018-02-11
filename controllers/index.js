c18c430fb36c19910e2dfe93737f656f82d8fbd7
Amy Qiu <amy-qiu@outlook.com>
2018-02-11 10:05:44 -0500

    Created Users schema and CRUD endpoints

--- controllers/index.js
+++ controllers/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 
 router.use('/faq', require('./Faq'));
 router.use('/activities', require('./Activities'));
+router.use('/users', require('./Users'));
 
 router.get('/', function(req, res) {
 	res.send("bacon!");
