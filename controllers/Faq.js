ade987f995360c0d33587e3d8c06792a015161ce
raphael <raph.koh@gmail.com>
2018-02-17 20:05:48 -0500

    Add FAQ endpoint to retrieve single FAQ

--- controllers/Faq.js
+++ controllers/Faq.js
@@ -8,7 +8,16 @@ faqRouter.get('/list', function(req, res) {
 		faqs = faqs.map(q => q.toJSONFor());
 		res.json(faqs);
 	});
-})
+});
+
+faqRouter.get('/id/:id', function(req, res) {
+  const id = req.params.id;
+  Faq.findById(id, function(err, faq) {
+    if (err) return res.status(500).json(err);
+    if (!faq) return res.json("Faq not found!");
+    res.json(faq);
+  });
+});
 
 faqRouter.delete('/delete', function(req, res) {
 	const ids = req.body.faqIDs.map(function(id) {
@@ -19,7 +28,7 @@ faqRouter.delete('/delete', function(req, res) {
 		if (err) return res.send(err);
 	});
 	res.send('Deleted questions!');
-})
+});
 
 faqRouter.post('/insert', function(req, res) {
 	var faq = new Faq();
