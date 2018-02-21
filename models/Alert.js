e1f9a1cb6b93c742eef7145190054e7307a9f2bb
pratyushpal <pratyush98pal@gmail.com>
2018-02-21 03:00:55 -0500

    Removed timestamp and ids from schema

--- models/Alert.js
+++ models/Alert.js
@@ -1,20 +1,17 @@
 var mongoose = require('mongoose');
 
 var AlertSchema = new mongoose.Schema({
-  id: Number,
   name: String,
   description: String,
-  timestamp: Date,
   isSmsSent: Boolean
 }, {timestamps: true});
 
 // Requires population of author
 AlertSchema.methods.toJSONFor = function() {
   return {
-    id: this.id,
+    id: this._id,
     name: this.name,
     description: this.description,
-    timestamp: this.timestamp,
     isSmsSent: this.isSmsSent
   };
 };
